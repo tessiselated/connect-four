1ae93327d3a660684c70ba75a6e158c707f830f2
Tess Elation <fredisded@gmail.com>
2016-12-02 11:37:25 +1100

    Added a feature where hovering the mouse shows where the next game piece would be played

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,3 @@
-
-
-
 var boardDOMElement = document.getElementById("board");
 var resultDOMElement = document.getElementById("winner-text");
 var resetDOMElement = document.getElementById("reset-game");
@@ -8,17 +5,24 @@ var resetDOMElement = document.getElementById("reset-game");
 
 
 
-var allianceIds = [];
-var imperialIds = [];
+
 
 var rows = 6;
 var columns = 7;
 
-var clickerCount = 0;
-var gameState = true;
-var gameWinner;
-var winSize = 4;
+// column and row variables set the size of the board
+// current iteration supports board sie up to 10 x 10
+// small change required to change to booard up tp 100 x 100
 
+var clickerCount = 0; //Keeps track of whose turn it is
+var gameState = true; //gameState true means that game is playable - ie not won or drawn
+var gameWinner; //stores the winner
+var winSize = 4; //how many matches constitute a win
+var allianceIds = [];
+var imperialIds = [];
+
+//createBoard draws the game board. It also resets game variables and is called when
+// the game is reset
 
 function createBoard() {
     boardDOMElement.innerHTML = "";
@@ -48,9 +52,10 @@ function createBoard() {
 createBoard();
 
 
+// startBottomRow adds the class empty to all blocks on the bottom row
 
 function startBottomRow() {
-    for (i=0; i < columns; i++) {
+    for (i = 0; i < columns; i++) {
         var bottomBlock = rows.toString() + i.toString();
         document.getElementById(bottomBlock).classList.add("empty");
 
@@ -62,8 +67,10 @@ function startBottomRow() {
 // Click event should change class of box clicked on - but only if class is empty
 
 
+// This is a potential target for refactor as there is some repeated code
 
 boardDOMElement.addEventListener("click", function() {
+
     var allianceCombos = [];
     var imperialCombos = [];
     if ((clickerCount % 2 === 0) && gameState === true) {
@@ -79,7 +86,7 @@ boardDOMElement.addEventListener("click", function() {
 
 
     } else if ((clickerCount % 2 !== 0) && gameState === true) {
-        turnStructure("alliance")
+        turnStructure("alliance");
         generateOwnedArrays();
         if (allianceIds.length >= winSize) {
             getCombinations(allianceIds, winSize, 0, [], allianceCombos);
@@ -88,7 +95,7 @@ boardDOMElement.addEventListener("click", function() {
         if (gameState === false) {
             gameWinner = "Alliance @";
         }
-        if (clickerCount === (rows*columns)) {
+        if (clickerCount === (rows * columns)) {
             gameState = false;
         }
     }
@@ -98,30 +105,33 @@ boardDOMElement.addEventListener("click", function() {
     }
 });
 
+// turn structure takes the argument of player based on which players turn it is
+
 function turnStructure(player) {
     var validPlay;
     var clickedBox = event.target;
     var clickedId = parseInt(event.target.id);
-    for (i=1; i <= rows; i++) {
+    for (i = 1; i <= rows; i++) {
         if (clickedBox.classList.contains("empty")) {
             clickedBox.classList.add(player, "in-play");
-            clickedBox.classList.remove("empty");
-            var validPlay = true;
+            clickedBox.classList.remove("empty", "alliance-hover", "imperial-hover");
+            validPlay = true;
             break;
-        }
-        else {
+        } else {
             clickedId = clickedId + 10;
-            clickedBox = document.getElementById(clickedId)
+            clickedBox = document.getElementById(clickedId);
         }
     }
     if (validPlay === true) {
-    clickerCount++;
-    stackPlay(clickedId);
-    validPlay = undefined;
-}
+        clickerCount++;
+        stackPlay(clickedId);
+        validPlay = undefined;
+    }
 
 
 }
+
+// stackPlay takes the ID of the last played box and makes the box above it playable
 
 
 function stackPlay(id) {
@@ -131,7 +141,7 @@ function stackPlay(id) {
     }
 }
 
-
+// printWinner displays the results of the game
 
 function printWinner() {
     if (gameWinner !== undefined) {
@@ -162,7 +172,10 @@ function generateOwnedArrays() {
 
     }
 }
-
+// getCombinations is a recursive function which is fairly complex
+// in english it grabs every possible combination of a particular size from elements in an array
+// this is used create an array containing arrays of every combination of elements in play
+// Win checks are then run on each of these arrays
 
 
 
@@ -273,3 +286,48 @@ function checkWin(arrayOfArrays) {
     checkForwardDiagonal(arrayOfArrays);
     checkBackDiagonal(arrayOfArrays);
 }
+
+
+boardDOMElement.addEventListener("mouseover", function() {
+    if (gameState === true) {
+        var hoveredBox = event.target;
+        var hoveredId = parseInt(event.target.id);
+        var i = hoveredId;
+        for (i; i <= 66; i += 10) {
+            if (hoveredBox.classList.contains("empty")) {
+                if (clickerCount % 2 === 0) {
+                    hoveredBox.classList.add("imperial-hover");
+                } else if (clickerCount % 2 !== 0) {
+                    hoveredBox.classList.add("alliance-hover");
+                }
+                break;
+            } else {
+                hoveredId = hoveredId + 10;
+                hoveredBox = document.getElementById(hoveredId);
+            }
+        }
+    }
+});
+
+boardDOMElement.addEventListener("mouseout", function() {
+    var hoveredBox = event.target;
+    var hoveredId = parseInt(event.target.id);
+    var i = hoveredId;
+
+    if (gameState === true) {
+        for (i; i <= 66; i += 10) {
+        if (hoveredBox.classList.contains("empty")) {
+            if (clickerCount % 2 === 0) {
+                hoveredBox.classList.remove("imperial-hover");
+            } else if (clickerCount % 2 !== 0) {
+                hoveredBox.classList.remove("alliance-hover");
+            }
+            break;
+        } else {
+            hoveredId = hoveredId + 10;
+            hoveredBox = document.getElementById(hoveredId);
+        }
+    }
+}
+
+});
